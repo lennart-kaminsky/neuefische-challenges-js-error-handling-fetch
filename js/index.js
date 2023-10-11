5241872eadf3d158978f27a67522da2819899bc9
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2023-10-11 12:41:54 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorElement = document.querySelector(".error");
 
 async function loadUser(url) {
   console.log(url);
   const response = await fetch(url);
-  const json = await response.json();
-  const user = json.data;
+  if (!response.ok) {
+    errorElement.textContent = "Network Error!";
+    userElement.innerHTML = "";
+    // throw new Error("Network Error");
+    // console.log("Network error");
+    return null; //default
+  }
+  try {
+    const json = await response.json();
+    const user = json.data;
+    userElement.innerHTML = `
+    <h2>${user.first_name} ${user.last_name}</h2>
+    <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+    `;
 
-  userElement.innerHTML = `
-  <h2>${user.first_name} ${user.last_name}</h2>
-  <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
-  `;
+    errorElement.innerHTML = "";
+  } catch (error) {
+    console.log(error);
+    userElement.innerHTML = "";
+    errorElement.textContent = "Error parsing JSON";
+  }
 }
 
 document
